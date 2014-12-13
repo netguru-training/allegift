3b2f01c61251ab3450fe11aa8c76fe28059b73b9
Daniel Koza <dakolech@gmail.com>
2014-12-13 14:47:10 +0100

    add pagination

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -7,7 +7,8 @@ class GiftsController < ApplicationController
   end
 
   def index
-    @gifts = Gift.all
+    #@gifts = Gift.all
+    @gifts = Gift.all.paginate(page: params[:page], per_page: 10)
   end
 
   def create
