28b3b812784c4d0ce1234658cc55bcccd6653015
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-12-13 12:08:16 +0100

    Add omniauth data to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141213102513) do
+ActiveRecord::Schema.define(version: 20141213110358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gifts", force: true do |t|
     t.string   "name"
-    t.string   "allegro_link"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "names", force: true do |t|
     t.string   "allegro_link"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -44,6 +37,9 @@ ActiveRecord::Schema.define(version: 20141213102513) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
