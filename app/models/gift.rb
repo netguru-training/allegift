e12589a5c0b6ed31147e86c1ba92b444fccacb29
konrad <konrad@debian>
2014-12-13 12:17:01 +0100

    Fetch offer ID from URL fix

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -3,6 +3,6 @@ class Gift < ActiveRecord::Base
 
   def fetch_id_from_link
     parts = allegro_link.split('i');
-    allegro_id = parts.last.split('.').first;
+    self.allegro_id = parts.last.split('.').first;
   end
 end
