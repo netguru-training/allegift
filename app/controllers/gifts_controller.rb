03b9bcd86bbf1ac2c4a2d686c6b4d8a33ace7037
Daniel Koza <dakolech@gmail.com>
2014-12-13 10:38:36 +0100

    add gifts controller

---
+++ app/controllers/gifts_controller.rb
@@ -0,0 +1,19 @@
+class GiftsController < ApplicationController
+  def add
+  end
+
+  def index
+  end
+
+  def create
+    self.gift = Gift.new(gift_params)
+
+    if gift.save
+      redirect_to gifts_index_path
+    else
+      render action: 'new'
+    end
+  end
+
+
+end
