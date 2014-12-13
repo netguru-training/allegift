5fe1eef888515f8da1938d56b8db6f15a0c94f82
Maciej Radziwon <maciejradziwon1987@gmail.com>
2014-12-13 12:34:31 +0100

    Finish fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+      generate_pass = SecureRandom.hex(8)
+      user.password =  generate_pass
+      user.password_confirmation = generate_pass
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   resources :gifts, only: [:add, :create, :index, :delete]
 
+  get 'auth/:provider/callback' => 'sessions#create', as: 'signin'
+  post 'signout' => 'sessions#destroy', as: 'signout'
+
   root 'welcome#index'
 end
