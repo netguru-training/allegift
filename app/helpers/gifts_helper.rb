85ba1f1797730d430a1ee0c872540a08285f3646
konrad <konrad@debian>
2014-12-14 14:01:08 +0100

    time to end column on wish list

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,2 +1,18 @@
 module GiftsHelper
+
+  NO_DUE_DATE = '---'
+  ENDED = 'ended'
+
+  def time_until_end(gift)
+    unless gift.due_date.nil?
+      if gift.due_date - Time.now > 0
+        return distance_of_time_in_words(Time.now, gift.due_date)
+      else
+        return ENDED
+      end
+    end
+    return NO_DUE_DATE
+  end
+
+
 end
